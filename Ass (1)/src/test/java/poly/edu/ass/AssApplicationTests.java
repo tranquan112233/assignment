af7bc81a1276fa8e2140cf54fe4d2d0388f2111d
Bfat-6z <minhhieu2007a@gmail.com>
2025-10-17 22:26:16 +0700

    fe-user

---
+++ Ass (1)/src/test/java/poly/edu/ass/AssApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.edu.ass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
